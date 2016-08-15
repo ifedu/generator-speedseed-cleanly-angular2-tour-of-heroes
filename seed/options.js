ab20ab057726ab51f43e98eb62bf04b0f0ceeba7
Eduardo Pérez-Bermejo <ifedu@outlook.com>
2016-08-15 02:02:15 +0200

    v.0.2.0

---
+++ seed/options.js
@@ -0,0 +1,19 @@
+module.exports = {
+    framework: [
+        { name: 'Angular2', value: 'angular2' }
+    ],
+
+    angular2: {
+        compiler: [
+            { name: 'TypeScript', value: 'typescript' }
+        ],
+
+        css: [
+            { name: 'SaSS', value: 'sass' }
+        ]
+    },
+
+    test: [
+        { name: 'Mocha', value: 'mocha' }
+    ]
+}
--- seed/template/angular2/compiler/typescript/app/js/main.ts
+++
@@ -1,4 +0,0 @@
-import { bootstrap } from '@angular/platform-browser-dynamic'
-import { AppComponent } from './../components/app/main'
-
-bootstrap(AppComponent)
